9d35a6c0e09a7b21fae4c13fe115fdbf45e31d96
Kais Bettaieb <kais.bettaieb@gmail.com>
2018-10-24 00:39:21 +0100

    update get user
    worked around the lazy hibernate error

--- JAVAEE-ejb/src/main/java/EPIONE/JAVAEE/services/implementation/UserService.java
+++ JAVAEE-ejb/src/main/java/EPIONE/JAVAEE/services/implementation/UserService.java
@@ -1,8 +1,92 @@
 package EPIONE.JAVAEE.services.implementation;
 
-import javax.ejb.Stateless;
+import EPIONE.JAVAEE.entities.Address;
+import EPIONE.JAVAEE.entities.Conversation;
+import EPIONE.JAVAEE.entities.User;
+import EPIONE.JAVAEE.services.interfaces.UserServiceRemote;
 
-@Stateless
-public class UserService {
-    
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Base64;
+import java.util.List;
+
+@Stateful
+public class UserService implements UserServiceRemote {
+    @PersistenceContext(unitName = "JAVAEE-ejb")
+    EntityManager em;
+
+    @Override
+    public int addUser(User user) {
+        user.setConfirmation("0");
+        user.setAddress(null);
+        user.setEnabled(true);
+        String token = Base64.getEncoder().encodeToString(user.getUsername().getBytes()) + Base64.getEncoder().encodeToString(user.getPassword().getBytes()) + Base64.getEncoder().encodeToString(user.getEmail().getBytes());
+        user.setConfirmationToken(token);
+        em.persist(user);
+        return user.getId();
+    }
+
+    @Override
+    public boolean activateUser(String activationToken) {
+        try {
+            User usr = (User) em.createQuery(
+                    "SELECT u FROM User u WHERE u.confirmationToken = :token")
+                    .setParameter("token", activationToken)
+                    .getSingleResult();
+            usr.setConfirmation("1");
+            em.merge(usr);
+            return true;
+        } catch (javax.persistence.NoResultException exp) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean deleteUser(int id) {
+        try {
+            User usr = (User) em.createQuery("SELECT u FROM User u WHERE u.id = :id")
+                    .setParameter("id", id)
+                    .getSingleResult();
+            usr.setEnabled(false);
+            em.merge(usr);
+            return true;
+        } catch (javax.persistence.NoResultException exp) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean addAddress(Address address) {
+        try {
+            User usr = (User) em.createQuery("SELECT u FROM User u WHERE u.id = :id")
+                    .setParameter("id", address.getUser().getId())
+                    .getSingleResult();
+            usr.setAddress(address);
+            em.persist(address);
+            em.merge(usr);
+            return true;
+        } catch (javax.persistence.NoResultException exp) {
+            return false;
+        }
+    }
+
+    @Override
+    public User getUserById(int id) {
+        try {
+            User usr = (User) em.createQuery("SELECT u FROM User u WHERE u.id = :id")
+                    .setParameter("id", id)
+                    .getSingleResult();
+            // bech matsirch lazy hibernate exception
+            if (!usr.getConversations().isEmpty())
+                usr.setConversations(usr.getConversations());
+
+            if (!usr.getMessageDoctors().isEmpty())
+                usr.setMessageDoctors(usr.getMessageDoctors());
+
+                return usr;
+        } catch (javax.persistence.NoResultException exp) {
+            return null;
+        }
+    }
 }
--- JAVAEE-ejb/src/main/java/EPIONE/JAVAEE/services/interfaces/UserServiceRemote.java
+++ JAVAEE-ejb/src/main/java/EPIONE/JAVAEE/services/interfaces/UserServiceRemote.java
@@ -1,7 +1,20 @@
 package EPIONE.JAVAEE.services.interfaces;
 
+import EPIONE.JAVAEE.entities.Address;
+import EPIONE.JAVAEE.entities.User;
+
 import javax.ejb.Remote;
 
 @Remote
 public interface UserServiceRemote {
+    public int addUser(User user);
+
+    public boolean activateUser(String activationToken);
+
+    public boolean deleteUser(int id);
+
+    public boolean addAddress(Address address);
+
+    public User getUserById(int id);
+
 }
