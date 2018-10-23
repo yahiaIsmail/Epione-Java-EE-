a0fe62ad96335587e2265343df8ed811879390a7
chaimaB <chaima.braiek@esprit.tn>
2018-10-23 18:50:28 +0100

    Revert "PathService"
    This reverts commit 38c715db8d807f13d2a34ba1f8a52de9b34dcea8.

--- JAVAEE-ejb/src/main/java/EPIONE/JAVAEE/services/MedicalPathService.java
+++ JAVAEE-ejb/src/main/java/EPIONE/JAVAEE/services/MedicalPathService.java
@@ -1,19 +1,4 @@
 package EPIONE.JAVAEE.services;
 
-import EPIONE.JAVAEE.entities.MedicalPath;
-
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-@Stateless
-public class MedicalPathService implements MedicalPathServiceInterface {
-    @PersistenceContext
-    private EntityManager em;
-
-
-    @Override
-    public int createMedicalPath(MedicalPath mediPath) {
-        em.persist(mediPath);
-        return mediPath.getId();
-    }
+public class MedicalPathService {
 }
--- JAVAEE-ejb/src/main/java/EPIONE/JAVAEE/services/MedicalPathServiceInterface.java
+++ JAVAEE-ejb/src/main/java/EPIONE/JAVAEE/services/MedicalPathServiceInterface.java
@@ -1,7 +1,4 @@
 package EPIONE.JAVAEE.services;
 
-import EPIONE.JAVAEE.entities.MedicalPath;
-
 public interface MedicalPathServiceInterface {
-    int createMedicalPath(MedicalPath mediPath);
 }
