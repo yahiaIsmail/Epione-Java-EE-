2ba0e1173c29f291131a09bb51775b905df27322
Kais Bettaieb <kais.bettaieb@gmail.com>
2018-10-29 20:32:06 +0100

    mise a jour

--- JAVAEE-ejb/src/main/java/EPIONE/JAVAEE/services/implementation/RdvJourService.java
+++ JAVAEE-ejb/src/main/java/EPIONE/JAVAEE/services/implementation/RdvJourService.java
@@ -3,13 +3,10 @@ package EPIONE.JAVAEE.services.implementation;
 import EPIONE.JAVAEE.entities.RDV;
 import EPIONE.JAVAEE.services.interfaces.RdvJourServiceLocal;
 
-
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.util.Collection;
 
 
@@ -26,7 +23,7 @@ public class RdvJourService implements RdvJourServiceLocal {
 
     @Override
     public Collection<RDV> ProgrammeJourneJ(int docId,int year,int month,int day) {
-      /*  Query query = em.createQuery("SELECT r FROM RDV r WHERE r.doctors.id =:d and YEAR(r.dateRDV)=:year and MONTH(r.dateRDV)=:month and DAY(r.dateRDV)=:day");
+        Query query = em.createQuery("SELECT r FROM RDV r WHERE r.doctors.id =:d and YEAR(r.dateRDV)=:year and MONTH(r.dateRDV)=:month and DAY(r.dateRDV)=:day");
         query.setParameter("day", day);
         query.setParameter("month", month);
         query.setParameter("year", year);
@@ -35,47 +32,8 @@ public class RdvJourService implements RdvJourServiceLocal {
         if (lst.size()==0)
             return null;
         else
-            return lst;*/
-      return null;
+            return lst;
     }
 
 
-    @Override
-    public void generateExcel(int docId) throws FileNotFoundException {
-
-       /* Query query = em.createQuery("SELECT r FROM RDV r WHERE r.doctors.id =:d ");
-        query.setParameter("d",docId);
-        Collection<RDV> lst = (Collection<RDV>) query.getResultList();
-
-        Document document = new Document();
-        PdfPTable table = new PdfPTable(new float[] { 2, 1, 2 });
-        table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
-        table.addCell("Id");
-        table.addCell("Date");
-        table.addCell("My confirmation");
-        table.addCell("Patient confirmation");
-        table.addCell("Patient confirmation");
-        table.addCell("Status");
-        table.addCell("Patient first name");
-        table.addCell("Patient last name");
-        table.setHeaderRows(1);
-        PdfPCell[] cells = table.getRow(0).getCells();
-        for (int j=0;j<cells.length;j++){
-            cells[j].setBackgroundColor(BaseColor.GRAY);
-        }
-
-        for(RDV rdv: lst) {
-            table.addCell(String.valueOf(rdv.getId()));
-            table.addCell(String.valueOf(rdv.isConfirmationDoc()));
-            table.addCell(String.valueOf(rdv.isConfirmationPatient()));
-            table.addCell(String.valueOf(rdv.getStatus()));
-            table.addCell(rdv.getUsers().getFirstName());
-        }
-        PdfWriter.getInstance(document, new FileOutputStream("sample3.pdf"));
-        document.open();
-        document.add(table);
-        document.close();
-        System.out.println("Done");*/
-
-    }
 }
