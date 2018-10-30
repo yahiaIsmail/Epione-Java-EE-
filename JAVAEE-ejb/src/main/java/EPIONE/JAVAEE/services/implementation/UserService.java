3e36e046e98ca5120a927f045c8d998337834f3b
Kais Bettaieb <kais.bettaieb@gmail.com>
2018-10-30 17:28:04 +0100

    moved rdv relatedfunction to seperate files

--- JAVAEE-ejb/src/main/java/EPIONE/JAVAEE/services/implementation/UserService.java
+++ JAVAEE-ejb/src/main/java/EPIONE/JAVAEE/services/implementation/UserService.java
@@ -271,7 +271,7 @@ public class UserService implements UserServiceLocal, UserServiceRemote {
         doctor.setTariff(tariff.replaceAll("Voir les tarifs", ""));
         doctor.setPaimentMethode(moyenPaiement);
         doctor.setLanguage(langues.replaceAll("Langues parlées", ""));
-        doctor.setUsername(firstName+"-"+lastName);
+        doctor.setUsername(firstName + "-" + lastName);
         doctor.setRole(Roles.Doctor);
         doctor.setAddress(addressSplited);
         em.persist(doctor);
@@ -320,7 +320,6 @@ public class UserService implements UserServiceLocal, UserServiceRemote {
     }
 
 
-
     @Override
     public boolean activatePatient(String activationToken) {
         try {
@@ -337,102 +336,25 @@ public class UserService implements UserServiceLocal, UserServiceRemote {
     }
 
     @Override
-    public int takeRvdPatient(String emailPatient, String emailDoctor, int motifId, int year, int month,
-                              int day, int hour, int minutes) {
-        try {
-            User patient = (User) em.createQuery(
-                    "SELECT u FROM User u WHERE u.email = :emailPatient")
-                    .setParameter("emailPatient", emailPatient)
-                    .getSingleResult();
-            User doctor = (User) em.createQuery(
-                    "SELECT u FROM User u WHERE u.email = :emailDoctor")
-                    .setParameter("emailDoctor", emailDoctor)
-                    .getSingleResult();
-            Address doctorAddress = doctor.getAddress();
-
-            Motif motif = (Motif) em.createQuery(
-                    "SELECT m FROM Motif m WHERE m.id = :id")
-                    .setParameter("id", motifId)
-                    .getSingleResult();
-            RDV rdv = new RDV();
-            Instant now = Instant.now();
-            Timestamp dateRdv = Timestamp.from(now);
-            dateRdv.setHours(hour);
-            dateRdv.setMinutes(minutes);
-            dateRdv.setMonth(month);
-            dateRdv.setYear(year);
-            dateRdv.setDate(day);
-
-            rdv.setMotif(motif);
-            rdv.setDateRDV(dateRdv);
-            rdv.setConfirmationDoc(false);
-            rdv.setConfirmationPatient(false);
-            rdv.setStatus(Status.InProgress);
-            rdv.setUsers(patient);
-            rdv.setDoctors(doctor);
-
-            em.persist(rdv);
-            return 1;
-        } catch (javax.persistence.NoResultException exp) {
-            return 0;
-        }
-    }
-    @Override
-    public boolean login(User u) throws Exception{
+    public boolean login(User u) throws Exception {
         // System.out.println(u.getPassword() + "password ");
         //  System.out.println(new String(Base64.getDecoder().decode(u.getPassword()),"UTF-8") + " password dencrypted");
         //byte[] decode= Base64.getDecoder().decode(u.getPassword());
-        String encode=Base64.getEncoder().encodeToString(u.getPassword().getBytes());
-        System.out.println("encode: "+ encode) ;
-        System.out.println("Login from service : "+u);
+        String encode = Base64.getEncoder().encodeToString(u.getPassword().getBytes());
+        System.out.println("encode: " + encode);
+        System.out.println("Login from service : " + u);
         Query query = em.createQuery("SELECT u FROM User u WHERE u.username = :username "
                 + "AND u.password = :password");
         query.setParameter("username", u.getUsername());
         query.setParameter("password", encode);
         // query.setParameter("password", decode);
         int resultCount = query.getResultList().size();
-        System.out.println("Found "+resultCount+" Result(s) ");
-        if(resultCount != 1){
+        System.out.println("Found " + resultCount + " Result(s) ");
+        if (resultCount != 1) {
             return false;
         }
         return true;
 
-    }
-
-    @Override
-    public boolean confirmRdvPatient(String token) {
-        try {
-            User usr = (User) em.createQuery(
-                    "SELECT u FROM User u WHERE u.confirmationToken = :token")
-                    .setParameter("token", token)
-                    .getSingleResult();
-            RDV rdv = (RDV) em.createQuery("SELECT rdv FROM RDV rdv WHERE rdv.users = : id")
-                    .setParameter("id",usr.getId())
-                    .getSingleResult();
-            rdv.setConfirmationPatient(true);
-            em.merge(rdv);
-            return true;
-        } catch (javax.persistence.NoResultException exp) {
-            return false;
-        }
-    }
-
-    @Override
-    public boolean confirmRdvDoctor(String token) {
-        try {
-            User usr = (User) em.createQuery(
-                    "SELECT u FROM User u WHERE u.confirmationToken = :token")
-                    .setParameter("token", token)
-                    .getSingleResult();
-            RDV rdv = (RDV) em.createQuery("SELECT rdv FROM RDV rdv WHERE rdv.users = : id")
-                    .setParameter("id",usr.getId())
-                    .getSingleResult();
-            rdv.setConfirmationDoc(true);
-            em.merge(rdv);
-            return true;
-        } catch (javax.persistence.NoResultException exp) {
-            return false;
-        }
     }
 }
 
--- JAVAEE-web/src/main/java/EPIONE/JAVAEE/presentation/mbeans/resource/UserResource.java
+++ JAVAEE-web/src/main/java/EPIONE/JAVAEE/presentation/mbeans/resource/UserResource.java
@@ -202,46 +202,5 @@ public class UserResource {
         return "failed";
     }
 
-    @POST
-    @Path("/takeRdv/{emailPatient}/{emailDoctor}/{motifId}/{year}/{month}/{day}/{hour}/{minutes}")
-    @Consumes(MediaType.TEXT_PLAIN)
-    @Produces(MediaType.APPLICATION_JSON)
-    public String takeRdv(@PathParam(value = "emailPatient") String emailPatient, @PathParam(value = "emailDoctor") String emailDoctor, @PathParam(value = "motifId") int motifId, @PathParam(value = "year") int year, @PathParam(value = "month") int month, @PathParam(value = "hour") int day, @PathParam(value = "hour") int hour, @PathParam(value = "minutes") int minutes) {
-        int takeRdvResponse = userServiceLocal.takeRvdPatient(emailPatient, emailDoctor, motifId, year, month, day, hour, minutes);
-        String responce = "failed";
-        if (takeRdvResponse != 0) {
-            String bodyPatient = "This mail is Sent to patient to inform him that he took a rendez-vous<br>, " +
-                    "Please confirm your attendency by clicking link below.<br>" +
-                    "<a href=></a>";
-            String bodyDoctor = "This mail is Sent to Doctor to inform him that a Patient he took a rendez-vous<br>," +
-                    "                    Please confirm your attendency by clicking link below.<br>" +
-                    "                    <a href=></a>";
-            if (SendMail.mail(emailPatient, "Confirm RDV", bodyPatient))
-                responce = "sent";
-            else
-                responce = "failed to send email patient";
-            if (SendMail.mail(emailDoctor, "Confirm RDV", bodyDoctor))
-                responce = "sent";
-            else
-                responce = "failed to send email doctor";
-        }
-        return responce;
-    }
 
-    @GET
-    @Path("user/confirmRDV")
-    @Produces(MediaType.APPLICATION_JSON)
-    public String confirmRdvPatient(@DefaultValue("empty") @QueryParam(value = "Token") String token) {
-        if (userServiceLocal.confirmRdvPatient(token))
-            return "confirmed";
-        return "failed";
-    }
-    @GET
-    @Path("doctor/confirmRDV")
-    @Produces(MediaType.APPLICATION_JSON)
-    public String confirmRdvDoctor(@DefaultValue("empty") @QueryParam(value = "Token") String token) {
-        if (userServiceLocal.confirmRdvDoctor(token))
-            return "confirmed";
-        return "failed";
-    }
 }
